6dd7b3a5032c01b7a91b9b563ffcc4c63a16eac5
snoupyrelete <drob8896@740s-pl-112.local>
2016-10-03 08:29:39 -0600

    Created, setup, tableviewController setup programmatically and linked to storyboard

---
+++ aboutMeV2.0/TableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aboutMeV2.0
+//
+//  Created by Robson, Dylan on 10/3/16.
+//  Copyright © 2016 Robson, Dylan. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
