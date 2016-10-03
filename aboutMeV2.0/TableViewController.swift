8be6278db84174922787731a9b734968f8c0424e
snoupyrelete <drob8896@740s-pl-112.local>
2016-10-03 09:06:05 -0600

    Setup tableview delegate functions

--- aboutMeV2.0/TableViewController.swift
+++ aboutMeV2.0/TableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    let myInfo = ["My hobbies","Facts about me","Why I think programming is important","Why I chose programming"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,18 @@ class TableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myInfo.count
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel!.text = myInfo[0]
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("hi")
+    }
 }
 
